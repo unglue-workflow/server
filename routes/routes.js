cab6b827ae711de20ce62f723327ddaa7888a628
Marc Stampfli <marc@zephir.ch>
2018-12-09 16:34:09 +0100

    Optimized routing

--- routes/routes.js
+++ routes/routes.js
@@ -4,28 +4,28 @@ const router = function (app) {
     const JsController = require('../controller/JsController');
     const SvgSpriteController = require('../controller/SvgSpriteController');
 
+    const compile = (controllerName, req, res) => {
+        let Controller = require('../controller/' + controllerName);
+        let Controller = new Controller(req, res);
+        return Controller.compile();
+    };
+
     app.get('/', function(req, res) {
         res.status(200).send('Available Endpoints (POST): /compile/scss, /compile/js');
     });
 
     app.post('/compile/css', function(req, res) {
-        const controller = new CssController(req, res);
-        const result = controller.compile();
-        
+        const result = compile('CssController', req, res);
         res.json(result);
     });
 
     app.post('/compile/js', function(req, res) {
-        const controller = new JsController(req, res);
-        const result = controller.compile();
-        
+        const result = compile('JsController', req, res);
         res.json(result);
     });
 
     app.post('/compile/svg-sprite', function(req, res) {
-        const controller = new SvgSpriteController(req, res);
-        const result = controller.compile();
-        
+        const result = compile('SvgSpriteController', req, res);
         res.json(result);
     });
     
